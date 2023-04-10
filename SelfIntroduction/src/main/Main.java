d2ea4b4972a9769154b3e2ed5f333548acc27490
potatomatop <potatomatop@192.168.0.10>
2023-04-10 23:56:47 +0900

    Lesson1_30 自己紹介①

--- SelfIntroduction/src/main/Main.java
+++ SelfIntroduction/src/main/Main.java
@@ -4,7 +4,8 @@ import sub.Person;
 
 class Main {
 	  public static void main(String[] args) {
-	    Person person1 = new Person("鈴木太郎", 20, 1.7, 60);		   
+// 問題5：Main.javaの引数にweightの60を入れてください
+	    Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
 	   
 //	    System.out.println(person1.name);
 //	    System.out.println(person1.age);
