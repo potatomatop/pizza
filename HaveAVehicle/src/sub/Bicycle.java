1a275cf950d691529151f8722719dc9d6baaca75
potatomatop <potatomatop@192.168.0.10>
2023-04-11 22:06:42 +0900

    Lesson1_31 VehicleとPersonの紐付け

---
+++ HaveAVehicle/src/sub/Bicycle.java
@@ -0,0 +1,6 @@
+package sub;
+
+// 問題2：CarクラスとBicycleクラスを作成しましょう。
+public class Bicycle extends Vehicle{
+
+}
---
+++ HaveAVehicle/src/sub/Car.java
@@ -0,0 +1,6 @@
+package sub;
+
+// 問題2：CarクラスとBicycleクラスを作成しましょう。
+public class Car extends Vehicle{
+
+}
